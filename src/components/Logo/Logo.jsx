1c6fb111186024e17a65e47395d420bab78a71c6
Ivan Zabolotniy <mustadonte@gmail.com>
2023-03-13 19:32:36 +0200

    fix logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -2,7 +2,7 @@ import logoImg from "../../img/logo.png";
 export const Logo = () => {
   return (
     <div className="logo">
-      <a href="/">
+      <a href="/rick-morty/">
         <img className="logo__img" src={logoImg} alt="Rick and Morty logo" />
       </a>
     </div>
